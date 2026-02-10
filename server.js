d81a3c16db7df5ef84e40fb88790a1d666a01ea4
ThizisRKP <editorsclubindia458@gmail.com>
2026-02-10 20:09:54 +0530

    PORT removed again

--- server.js
+++ server.js
@@ -57,6 +57,8 @@ const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
+    
+    
 
     const PORT = process.env.PORT || 5000;
 
